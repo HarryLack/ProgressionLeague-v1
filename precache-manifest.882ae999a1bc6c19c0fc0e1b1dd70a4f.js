abd87d90bf7e68e396f57a51ae9f598f58f10d11
HarryLack <harrylackdev@gmail.com>
2022-08-21 21:00:49 +0100

    Initial deployment

---
+++ precache-manifest.882ae999a1bc6c19c0fc0e1b1dd70a4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "376d10117bc7bf45d05e",
+    "url": "/ProgressionLeague/static/css/main.812e9ff7.chunk.css"
+  },
+  {
+    "revision": "376d10117bc7bf45d05e",
+    "url": "/ProgressionLeague/static/js/main.376d1011.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/ProgressionLeague/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/ProgressionLeague/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "58321cdcf255e589e0da",
+    "url": "/ProgressionLeague/static/js/runtime~main.58321cdc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ProgressionLeague/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "e9cc519f79bcbf0c2fb1e961dd3516b6",
+    "url": "/ProgressionLeague/index.html"
+  }
+];
