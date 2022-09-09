7d47efe6ea2b642fc6f74a1ac1db6975fe17e90d
HarryLack <harrylackdev@gmail.com>
2022-09-10 00:34:19 +0100

    v0.2.0 Updated to W5 list. Fixed prevStatus assignment

--- src/handlers/banlistHandler.ts
+++ src/handlers/banlistHandler.ts
@@ -1,7 +1,7 @@
 import { BanlistState, cardInfo, LimitType, setBanlist } from "../store/slices/banlistSlice";
 import { store } from "../store/store";
 
-async function ReadBanlistFile(newList?: boolean):Promise<any> {
+async function ReadBanlistFile(newList?: boolean): Promise<any> {
     const listSrc = newList ? "/lists/new/ProgressionList.json" : "/lists/prev/ProgressionList.json"
 
     const list = await fetch(process.env.PUBLIC_URL + listSrc, { method: "GET" }).then((response) => response.json())
@@ -10,15 +10,15 @@ async function ReadBanlistFile(newList?: boolean):Promise<any> {
 }
 
 async function ParseBanlists() {
-    let newList: Partial<Omit<BanlistState,"lastChanged">> = {}
-    await ReadBanlistFile(true).then((response: any) => {newList = response})
+    let newList: Partial<Omit<BanlistState, "lastChanged">> = {}
+    await ReadBanlistFile(true).then((response: any) => { newList = response })
     let oldList: Partial<Omit<BanlistState, "lastChanged">> = {};
     await ReadBanlistFile().then((response: any) => { oldList = response })
     return { newList, oldList }
 }
 export async function GenerateBanlist() {
     const database = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?startdate=01%2F01%2F1990&enddate=01%2F01%2F2100&dateregion=tcg_date', { method: 'GET' })
-        .then((response) => 
+        .then((response) =>
             response.json()
         ).catch((err) => console.log(err))
     const { newList, oldList } = await ParseBanlists();
@@ -34,18 +34,14 @@ export async function GenerateBanlist() {
             if (dbCard) {
                 const status = key
                 let prevStatus = "";
-                if (!oldList?.[key as keyof Omit<BanlistState, "lastChanged">]?.includes(id)) {
-                    Object.entries(oldList).forEach(([key, value]) => {
-                        if (key !== status && value.includes(id)) {
-                            prevStatus = LimitType[key]
-                        } else {
-                            prevStatus = "New";
-                        }
-                    })
-                }
+                Object.entries(oldList).forEach(([key, value]) => {
+                    if (value.includes(id)) {
+                        prevStatus = LimitType[key]
+                    }
+                })
                 //@ts-expect-error
-                banlist[key].push({ id: value, name: dbCard.name, type: dbCard.type, status: LimitType[status], prevStatus: prevStatus })
-            }else{
+                banlist[key].push({ id: value, name: dbCard.name, type: dbCard.type, status: LimitType[status], prevStatus: prevStatus ? prevStatus : "New" })
+            } else {
                 console.log("Card not found: ", id)
             }
         })
@@ -56,18 +52,17 @@ export async function GenerateBanlist() {
             //@ts-expect-error typing
             const dbCard = database && database.data.find(element => element.id == id);
             if (dbCard) {
-                const status = key;
+                let removed = true;
+                const status = key
                 let prevStatus = "";
-                if (!newList?.[key as keyof Omit<BanlistState, "lastChanged">]?.includes(id)) {
-                    Object.entries(newList).forEach(([key, value]) => {
-                        if (value.includes(id)) {
-
-                        } else {
-                            prevStatus = LimitType[status]
-                        }
-                    })
-                }
-                if (prevStatus) {
+                Object.entries(newList).forEach(([key, value]) => {
+                    if (value.includes(id)) {
+                        removed = false
+                    } else {
+                        prevStatus = LimitType[status]
+                    }
+                })
+                if (removed) {
                     //@ts-expect-error
                     banlist.removed.push({ id: value, name: dbCard.name, type: dbCard.type, status: LimitType["removed"], prevStatus: prevStatus })
                 }
