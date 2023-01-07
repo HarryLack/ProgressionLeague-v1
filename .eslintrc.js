8308c56000a35cbefeb845e43842ad1efe3c54a2
HarryLack <harrylackdev@gmail.com>
2023-01-07 22:16:01 +0000

    build: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript', 'react-app',
+    'react-app/jest',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
