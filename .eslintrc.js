83749a0f3561462b31e693b13876826019354930
HarryLack <harrylackdev@gmail.com>
2023-04-22 13:51:29 +0100

    chore: commit stuff to fix elsewhere

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,23 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard-with-typescript',
-    'plugin:import/typescript'
   ],
-  overrides: [
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+            extends: ['standard-with-typescript',
+                'plugin:import/typescript'],
+            parserOptions: {
+                project: ['./tsconfig.json'], // Specify it only for TypeScript files
+            },
+        },
   ],
   plugins: [
     'react'
   ],
   rules: {
     // Typescript cpvers this
-    'react/prop-types': 0
+    'react/prop-types': 0,
   },
   parser: '@babel/eslint-parser',
   parserOptions: {
